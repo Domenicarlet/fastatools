14da34b651d4acb6e25ef2a8cc0ef209305b0b06
Domenica <domenicarlet@gmail.com>
2025-10-31 13:01:17 +0100

    Homework Day 9

--- peek.sh
+++ peek.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-# Check that two arguments are provided
 if [ $# -lt 2 ]; then
   echo "Usage: bash peek.sh <filename> <num_lines>"
   exit 1
@@ -9,11 +6,9 @@ fi
 file=$1
 num=$2
 
-# Print first N lines
 head -n "$num" "$file"
 
 # Separator
 echo "..."
 
-# Print last N lines
 tail -n "$num" "$file"
